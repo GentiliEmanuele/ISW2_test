01dc38c44c25062bfcfb535a74f7d2eba0459288
GentiliEmanuele <egentili2000@gmail.com>
2024-03-25 16:57:40 +0100

    First commit

---
+++ src/main/java/com/gentili/app/App.java
@@ -0,0 +1,13 @@
+package com.gentili.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
